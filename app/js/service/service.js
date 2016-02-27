8eb3a273b18e39f0d31154e56ad1db2946cbb3f7
truan86 <truan@ukr.net>
2016-02-28 00:20:40 +0200

    fixed setting ships

--- app/js/service/service.js
+++ app/js/service/service.js
@@ -2,9 +2,16 @@ class GameService {
     constructor() {
         let fieldSize = 9;
         this.gameField = [];
+        this.enemyField = [];
         for (let i = 0; i <= fieldSize; i++) {
             for (let j = 0; j <= fieldSize; j++) {
                 this.gameField.push({
+                    "y": i,
+                    "x": j,
+                    "ship": false,
+                    "hover": false
+                });
+                this.enemyField.push({
                     "y": i,
                     "x": j,
                     "ship": false,
@@ -17,6 +24,18 @@ class GameService {
             "size": 1,
             "hide": false
         }, {
+            "name": "Small Ship",
+            "size": 1,
+            "hide": false
+        }, {
+            "name": "Small Ship",
+            "size": 1,
+            "hide": false
+        }, {
+            "name": "Middle Ship",
+            "size": 3,
+            "hide": false
+        }, {
             "name": "Middle Ship",
             "size": 3,
             "hide": false
@@ -25,6 +44,8 @@ class GameService {
             "size": 5,
             "hide": false
         }];
+
+
     }
 }
 
--- app/js/start/start.js
+++ app/js/start/start.js
@@ -2,6 +2,7 @@ class StartController {
     constructor(GameService) {
         this.gameService = GameService;
         this.gameField = GameService.gameField;
+        this.enemyField = GameService.enemyField;
         this.shipList = GameService.shipList;
         this.sizeShip = 0;
         this.TurnTheShip = false;
@@ -15,8 +16,21 @@ class StartController {
     hovered(id) {
         if (this.TurnTheShip === false) {
             if (+(this.gameField[id].x) + this.sizeShip < 11) {
-                for (let i = id; i < id + this.sizeShip; i++) {
-                    this.gameField[i].hover = true;
+                if (this.howCloseTheShip(id)) {
+                    for (let i = id; i < id + this.sizeShip; i++) {
+                        this.gameField[i].hover = true;
+                    }
+                }
+            }
+        }
+        else {
+            if (+(this.gameField[id].y) + this.sizeShip < 11) {
+                if (this.howCloseTheShip(id)) {
+                    let countCell = id;
+                    for (let i = 0; i < this.sizeShip; i++) {
+                        this.gameField[countCell].hover = true;
+                        countCell += 10;
+                    }
                 }
             }
         }
@@ -25,8 +39,21 @@ class StartController {
     lieved(id) {
         if (this.TurnTheShip === false) {
             if (+(this.gameField[id].x) + this.sizeShip < 11) {
-                for (let i = id; i < id + this.sizeShip; i++) {
-                    this.gameField[i].hover = false;
+                if (this.howCloseTheShip(id)) {
+                    for (let i = id; i < id + this.sizeShip; i++) {
+                        this.gameField[i].hover = false;
+                    }
+                }
+            }
+        }
+        else {
+            if (+(this.gameField[id].y) + this.sizeShip < 11) {
+                if (this.howCloseTheShip(id)) {
+                    let countCell = id;
+                    for (let i = 0; i < this.sizeShip; i++) {
+                        this.gameField[countCell].hover = false;
+                        countCell += 10;
+                    }
                 }
             }
         }
@@ -34,14 +61,54 @@ class StartController {
 
     setShip(id) {
         if (this.TurnTheShip === false) {
-            console.log(+(this.gameField[id].y));
             if (+(this.gameField[id].x) + this.sizeShip < 11) {
-                for (let i = id; i < id + this.sizeShip; i++) {
-                    this.gameField[i].ship = true;
+                if (this.howCloseTheShip(id)) {
+                    for (let i = id; i < id + this.sizeShip; i++) {
+                        this.gameField[i].ship = true;
+                    }
+                    this.sizeShip = 0;
+                    this.shipList[this.shipListId].hide = true;
                 }
-                this.sizeShip = 0;
-                this.shipList[this.shipListId].hide = true;
             }
+        }
+        else {
+            if (+(this.gameField[id].y) + this.sizeShip < 11) {
+                if (this.howCloseTheShip(id)) {
+                    let countCell = id;
+                    for (let i = 0; i < this.sizeShip; i++) {
+                        this.gameField[countCell].ship = true;
+                        countCell += 10;
+                    }
+                    this.sizeShip = 0;
+                    this.shipList[this.shipListId].hide = true;
+                }
+            }
+        }
+    }
+
+    howCloseTheShip(id) {
+        let CloseTheShip = 0;
+        this.gameField.forEach(function (item, i) {
+            if (i == id - 11 ||
+                i == id - 10 ||
+                i == id - 9 ||
+                i == id - 1 ||
+                i == id ||
+                i == id + 1 ||
+                i == id + 9 ||
+                i == id + 10 ||
+                i == id + 11
+            ) {
+                if (item.ship === true) {
+                    CloseTheShip += 1;
+                }
+            }
+        });
+        if (CloseTheShip == 0) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
